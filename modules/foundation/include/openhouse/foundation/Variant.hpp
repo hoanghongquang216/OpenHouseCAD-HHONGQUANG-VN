7d1b47ccabd7dc5662132c32ea088f91ac40ae8a
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:16:01 +0700

    foundation: add variant convenience header

---
+++ modules/foundation/include/openhouse/foundation/Variant.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <variant>
+
+namespace openhouse::foundation {
+
+using std::get;
+using std::holds_alternative;
+using std::monostate;
+using std::variant;
+using std::visit;
+
+}
