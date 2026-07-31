5fa24ef477954f7546af0738723eff5f22b51110
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:29:43 +0700

    foundation: add cstdint convenience header

---
+++ modules/foundation/include/openhouse/foundation/Cstdint.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+namespace openhouse::foundation {
+
+using std::int8_t;
+using std::int16_t;
+using std::int32_t;
+using std::int64_t;
+using std::uint8_t;
+using std::uint16_t;
+using std::uint32_t;
+using std::uint64_t;
+using std::intptr_t;
+using std::uintptr_t;
+
+}
