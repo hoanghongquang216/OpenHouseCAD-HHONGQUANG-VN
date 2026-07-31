7227330c05797f25fad5185051d0c3ffa8c8bc3b
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:14:44 +0700

    foundation: add containers convenience header

---
+++ modules/foundation/include/openhouse/foundation/Containers.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <array>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace openhouse::foundation {
+
+using std::array;
+using std::deque;
+using std::map;
+using std::set;
+using std::unordered_map;
+using std::unordered_set;
+using std::vector;
+
+}
