9c799ea2054e0a8d9b6e4dd37dfada7eee0d13d9
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:28:12 +0700

    foundation: add format convenience header

---
+++ modules/foundation/include/openhouse/foundation/Format.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <format>
+
+namespace openhouse::foundation {
+
+using std::format;
+using std::format_to;
+using std::formatted_size;
+using std::vformat;
+using std::make_format_args;
+
+}
