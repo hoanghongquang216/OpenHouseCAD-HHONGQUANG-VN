8a6503969748068106506b3288fe5210e2674f53
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:35:36 +0700

    foundation: add cmath convenience header

---
+++ modules/foundation/include/openhouse/foundation/CMath.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cmath>
+
+namespace openhouse::foundation {
+
+using std::abs;
+using std::sqrt;
+using std::sin;
+using std::cos;
+using std::tan;
+using std::asin;
+using std::acos;
+using std::atan2;
+using std::hypot;
+using std::floor;
+using std::ceil;
+using std::round;
+using std::pow;
+
+}
