1b1ea9ea5e6e949cb0bc66b9ef25cc47730ed0d7
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:23:51 +0700

    foundation: add bit convenience header

---
+++ modules/foundation/include/openhouse/foundation/Bit.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <bit>
+
+namespace openhouse::foundation {
+
+using std::bit_cast;
+using std::byteswap;
+using std::endian;
+using std::has_single_bit;
+using std::bit_ceil;
+using std::bit_floor;
+using std::bit_width;
+
+}
