4ae2bcd0d8c8ec113c5c58886ebc479bbf21a85e
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:17:20 +0700

    foundation: add functional convenience header

---
+++ modules/foundation/include/openhouse/foundation/Functional.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <functional>
+
+namespace openhouse::foundation {
+
+using std::function;
+using std::invoke;
+using std::reference_wrapper;
+using std::ref;
+using std::cref;
+
+}
