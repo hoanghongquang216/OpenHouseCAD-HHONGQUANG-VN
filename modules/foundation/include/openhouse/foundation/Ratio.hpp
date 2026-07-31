d3c7f96cae7630868357af6f13d3944fe501d56e
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:31:55 +0700

    foundation: add ratio convenience header

---
+++ modules/foundation/include/openhouse/foundation/Ratio.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <ratio>
+
+namespace openhouse::foundation {
+
+using std::ratio;
+using std::milli;
+using std::micro;
+using std::nano;
+using std::kilo;
+using std::mega;
+using std::giga;
+
+}
