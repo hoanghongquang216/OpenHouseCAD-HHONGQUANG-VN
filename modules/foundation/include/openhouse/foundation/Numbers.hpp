162c95b0dd08750b25875911c29160794d01d434
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:26:39 +0700

    foundation: add numbers convenience header

---
+++ modules/foundation/include/openhouse/foundation/Numbers.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <numbers>
+
+namespace openhouse::foundation {
+
+using std::numbers::e_v;
+using std::numbers::pi_v;
+using std::numbers::inv_pi_v;
+using std::numbers::sqrt2_v;
+using std::numbers::phi_v;
+
+}
