caa0e82df3984a5a76174defbb5dbfddc84812b3
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:34:18 +0700

    foundation: add charconv convenience header

---
+++ modules/foundation/include/openhouse/foundation/CharConv.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <charconv>
+
+namespace openhouse::foundation {
+
+using std::from_chars;
+using std::to_chars;
+using std::from_chars_result;
+using std::to_chars_result;
+
+}
