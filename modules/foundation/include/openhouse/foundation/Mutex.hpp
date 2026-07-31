466c37fde2ac03f8ec287707e7ec3c3f5432f51e
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:22:37 +0700

    foundation: add mutex convenience header

---
+++ modules/foundation/include/openhouse/foundation/Mutex.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <mutex>
+
+namespace openhouse::foundation {
+
+using std::lock_guard;
+using std::mutex;
+using std::recursive_mutex;
+using std::scoped_lock;
+using std::shared_mutex;
+using std::shared_lock;
+using std::unique_lock;
+
+}
