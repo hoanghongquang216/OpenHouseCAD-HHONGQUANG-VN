01fb24b62fc94ac3aa1b6b77e121e543de2fb0d1
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:29:00 +0700

    foundation: add random convenience header

---
+++ modules/foundation/include/openhouse/foundation/Random.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <random>
+
+namespace openhouse::foundation {
+
+using std::mt19937;
+using std::mt19937_64;
+using std::random_device;
+using std::uniform_int_distribution;
+using std::uniform_real_distribution;
+using std::normal_distribution;
+
+}
