500f2cc0fc8c5beadf2682c3757737fe23d27db1
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:21:42 +0700

    foundation: add thread convenience header

---
+++ modules/foundation/include/openhouse/foundation/Thread.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <thread>
+
+namespace openhouse::foundation {
+
+using std::jthread;
+using std::thread;
+using std::this_thread;
+using std::stop_source;
+using std::stop_token;
+
+}
