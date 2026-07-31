4c9580d411e2dcb7438f160d030ac7f981500ca2
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:13:22 +0700

    foundation: add memory convenience header

---
+++ modules/foundation/include/openhouse/foundation/Memory.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <memory>
+
+namespace openhouse::foundation {
+
+using std::make_shared;
+using std::make_unique;
+using std::shared_ptr;
+using std::unique_ptr;
+using std::weak_ptr;
+
+}
