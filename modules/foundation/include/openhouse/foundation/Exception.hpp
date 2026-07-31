321ea0d4c31c3185389ff372816bc38d182add38
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:27:23 +0700

    foundation: add exception convenience header

---
+++ modules/foundation/include/openhouse/foundation/Exception.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <exception>
+#include <stdexcept>
+
+namespace openhouse::foundation {
+
+using std::exception;
+using std::logic_error;
+using std::runtime_error;
+using std::invalid_argument;
+using std::out_of_range;
+
+}
