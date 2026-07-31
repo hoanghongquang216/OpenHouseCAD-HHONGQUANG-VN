77e127b0f22daa794e272eb04245e4dd9d6ffd18
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-07-31 22:31:11 +0700

    foundation: add iterator convenience header

---
+++ modules/foundation/include/openhouse/foundation/Iterator.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iterator>
+
+namespace openhouse::foundation {
+
+using std::advance;
+using std::distance;
+using std::next;
+using std::prev;
+using std::begin;
+using std::end;
+using std::cbegin;
+using std::cend;
+
+}
