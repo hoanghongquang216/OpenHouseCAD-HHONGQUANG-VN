3f5aeec1f45d1e6dcb38c78e79ceea4a7d0e63e6
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-08-02 05:16:42 +0700

    Add HalfEdge connectivity references

--- modules/kernel/include/openhouse/kernel/HalfEdge.hpp
+++ modules/kernel/include/openhouse/kernel/HalfEdge.hpp
@@ -5,6 +5,10 @@
 namespace openhouse::kernel
 {
 
+class Vertex;
+class Edge;
+class Loop;
+
 class HalfEdge : public TopologyEntity
 {
 public:
@@ -12,6 +16,41 @@ public:
         : TopologyEntity(id)
     {
     }
+
+    void SetVertex(Vertex* vertex)
+    {
+        vertex_ = vertex;
+    }
+
+    void SetEdge(Edge* edge)
+    {
+        edge_ = edge;
+    }
+
+    void SetLoop(Loop* loop)
+    {
+        loop_ = loop;
+    }
+
+    Vertex* VertexRef() const
+    {
+        return vertex_;
+    }
+
+    Edge* EdgeRef() const
+    {
+        return edge_;
+    }
+
+    Loop* LoopRef() const
+    {
+        return loop_;
+    }
+
+private:
+    Vertex* vertex_ = nullptr;
+    Edge* edge_ = nullptr;
+    Loop* loop_ = nullptr;
 };
 
 }
