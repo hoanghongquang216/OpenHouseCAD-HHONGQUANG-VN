26deeb2a0c4a2bbd769a091c40e359c0de8ea13f
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-08-02 05:20:36 +0700

    Add Face loop boundary foundation

--- modules/kernel/include/openhouse/kernel/Face.hpp
+++ modules/kernel/include/openhouse/kernel/Face.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <vector>
+
 #include <openhouse/kernel/TopologyEntity.hpp>
 
 namespace openhouse::kernel
 {
+
+class Loop;
 
 class Face : public TopologyEntity
 {
@@ -12,6 +16,19 @@ public:
         : TopologyEntity(id)
     {
     }
+
+    void AddLoop(Loop* loop)
+    {
+        loops_.push_back(loop);
+    }
+
+    const std::vector<Loop*>& Loops() const
+    {
+        return loops_;
+    }
+
+private:
+    std::vector<Loop*> loops_;
 };
 
 }
