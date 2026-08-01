8992ba5ad3c21c32a9213c673da6a58a75e9a320
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-08-02 06:58:32 +0700

    Add transaction ownership boundary test

---
+++ modules/kernel/tests/Test_TransactionOwnership.cpp
@@ -0,0 +1,16 @@
+#include <cassert>
+
+#include <openhouse/kernel/TransactionScope.hpp>
+
+int main()
+{
+    openhouse::kernel::TransactionScope scope;
+
+    assert(!scope.Current().IsCommitted());
+
+    scope.Commit();
+
+    assert(scope.Current().IsCommitted());
+
+    return 0;
+}
