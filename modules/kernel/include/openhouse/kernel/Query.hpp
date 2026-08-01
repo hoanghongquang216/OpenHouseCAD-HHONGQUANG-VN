5de73d5ee12b9b8c206812411e75b18a1b90e66f
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-08-02 06:00:49 +0700

    Add Query API facade foundation

---
+++ modules/kernel/include/openhouse/kernel/Query.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <openhouse/kernel/ObjectStoreFilter.hpp>
+#include <openhouse/kernel/ObjectStorePredicate.hpp>
+#include <openhouse/kernel/ObjectStoreQuery.hpp>
+
+namespace openhouse::kernel::Query
+{
+
+using openhouse::kernel::ForEach;
+using openhouse::kernel::FindAll;
+using openhouse::kernel::FindIf;
+
+}
