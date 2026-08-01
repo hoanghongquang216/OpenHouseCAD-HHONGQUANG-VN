35c5e9e0d07f1155df7391034f6fe3ef0156d803
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-08-02 05:24:06 +0700

    Refine ObjectStore lifecycle validation foundation

--- modules/kernel/include/openhouse/kernel/ObjectStore.hpp
+++ modules/kernel/include/openhouse/kernel/ObjectStore.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <memory>
+#include <stdexcept>
 #include <unordered_map>
+#include <utility>
 
 #include <openhouse/kernel/ObjectId.hpp>
 
@@ -9,6 +11,9 @@ namespace openhouse::kernel
 {
 
 class Entity;
+
+template<class T>
+class Handle;
 
 class ObjectStore
 {
@@ -18,16 +23,31 @@ public:
     template<class T, class... Args>
     T* Create(ObjectId id, Args&&... args)
     {
+        if (Contains(id))
+        {
+            throw std::runtime_error("ObjectId already exists");
+        }
+
         auto object = std::make_unique<T>(id, std::forward<Args>(args)...);
         auto pointer = object.get();
         objects_.emplace(id, std::move(object));
         return pointer;
     }
 
+    bool Contains(ObjectId id) const
+    {
+        return objects_.find(id) != objects_.end();
+    }
+
     Entity* Find(ObjectId id) const
     {
         auto iterator = objects_.find(id);
         return iterator == objects_.end() ? nullptr : iterator->second.get();
+    }
+
+    std::size_t Size() const
+    {
+        return objects_.size();
     }
 
 private:
