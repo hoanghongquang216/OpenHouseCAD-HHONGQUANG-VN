242930f472641e875a6da3409cdaf7eaf5b8a303
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-08-02 05:16:50 +0700

    Add Loop half edge boundary storage foundation

--- modules/kernel/include/openhouse/kernel/Loop.hpp
+++ modules/kernel/include/openhouse/kernel/Loop.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <vector>
+
 #include <openhouse/kernel/TopologyEntity.hpp>
 
 namespace openhouse::kernel
 {
+
+class HalfEdge;
 
 class Loop : public TopologyEntity
 {
@@ -12,6 +16,19 @@ public:
         : TopologyEntity(id)
     {
     }
+
+    void AddHalfEdge(HalfEdge* edge)
+    {
+        half_edges_.push_back(edge);
+    }
+
+    const std::vector<HalfEdge*>& HalfEdges() const
+    {
+        return half_edges_;
+    }
+
+private:
+    std::vector<HalfEdge*> half_edges_;
 };
 
 }
