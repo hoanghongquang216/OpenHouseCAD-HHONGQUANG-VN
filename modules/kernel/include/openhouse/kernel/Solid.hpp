2cfedb524fca7f0ed9a60e3cab2e0fc73cbc9df6
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-08-02 05:21:10 +0700

    Refine Solid as shell container foundation

--- modules/kernel/include/openhouse/kernel/Solid.hpp
+++ modules/kernel/include/openhouse/kernel/Solid.hpp
@@ -1,12 +1,34 @@
 #pragma once
 
-#include <openhouse/kernel/Entity.hpp>
+#include <vector>
+
+#include <openhouse/kernel/TopologyEntity.hpp>
 
 namespace openhouse::kernel
 {
-class Solid : public Entity
+
+class Shell;
+
+class Solid : public TopologyEntity
 {
 public:
-    explicit Solid(ObjectId id) : Entity(id) {}
+    explicit Solid(ObjectId id)
+        : TopologyEntity(id)
+    {
+    }
+
+    void AddShell(Shell* shell)
+    {
+        shells_.push_back(shell);
+    }
+
+    const std::vector<Shell*>& Shells() const
+    {
+        return shells_;
+    }
+
+private:
+    std::vector<Shell*> shells_;
 };
+
 }
