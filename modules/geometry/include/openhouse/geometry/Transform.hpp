dd9eb7a115e5e1092ccb2e0a08176491868c5e3a
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-08-02 09:59:40 +0700

    Implement Transform component foundation

--- modules/geometry/include/openhouse/geometry/Transform.hpp
+++ modules/geometry/include/openhouse/geometry/Transform.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <openhouse/geometry/Matrix4.hpp>
+#include <openhouse/geometry/Point3D.hpp>
 
 namespace openhouse::geometry
 {
@@ -8,7 +9,31 @@ namespace openhouse::geometry
 class Transform
 {
 public:
-    Transform() = default;
+    Transform()
+        : position_(0.0, 0.0, 0.0), matrix_(Matrix4::Identity())
+    {
+    }
+
+    explicit Transform(const Point3D& position)
+        : position_(position), matrix_(Matrix4::Translation(position.X(), position.Y(), position.Z()))
+    {
+    }
+
+    const Point3D& Position() const
+    {
+        return position_;
+    }
+
+    void SetPosition(const Point3D& position)
+    {
+        position_ = position;
+        matrix_ = Matrix4::Translation(position.X(), position.Y(), position.Z());
+    }
+
+    Point3D Apply(const Point3D& point) const
+    {
+        return matrix_.Transform(point);
+    }
 
     const Matrix4& Matrix() const
     {
@@ -16,6 +41,7 @@ public:
     }
 
 private:
+    Point3D position_;
     Matrix4 matrix_;
 };
 
