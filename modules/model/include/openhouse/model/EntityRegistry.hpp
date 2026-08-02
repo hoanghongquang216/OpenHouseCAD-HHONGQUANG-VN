816ef25e3d0119277996e3d91f9f07f1345dbeb9
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-08-02 08:15:06 +0700

    Add entity registry foundation

---
+++ modules/model/include/openhouse/model/EntityRegistry.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <unordered_map>
+
+#include <openhouse/model/Entity.hpp>
+
+namespace openhouse::model
+{
+
+class EntityRegistry
+{
+public:
+    bool Add(Entity entity)
+    {
+        const auto id = entity.Id().Value();
+
+        if (id == 0 || entities_.contains(id))
+        {
+            return false;
+        }
+
+        entities_.emplace(id, std::move(entity));
+        return true;
+    }
+
+    Entity* Find(EntityId id)
+    {
+        auto it = entities_.find(id.Value());
+
+        if (it == entities_.end())
+        {
+            return nullptr;
+        }
+
+        return &it->second;
+    }
+
+    const Entity* Find(EntityId id) const
+    {
+        auto it = entities_.find(id.Value());
+
+        if (it == entities_.end())
+        {
+            return nullptr;
+        }
+
+        return &it->second;
+    }
+
+    std::size_t Count() const
+    {
+        return entities_.size();
+    }
+
+private:
+    std::unordered_map<EntityIdValue, Entity> entities_;
+};
+
+}
