aac6409cc867142bf33d04d30087a52a8c48c6f9
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-08-02 08:49:05 +0700

    Complete point3 vector operations

--- modules/geometry/include/openhouse/geometry/Point3.hpp
+++ modules/geometry/include/openhouse/geometry/Point3.hpp
@@ -22,6 +22,19 @@ public:
         return Point3(x_ + vector.X(), y_ + vector.Y(), z_ + vector.Z());
     }
 
+    constexpr Point3 operator-(const Vector3& vector) const
+    {
+        return Point3(x_ - vector.X(), y_ - vector.Y(), z_ - vector.Z());
+    }
+
+    constexpr Vector3 operator-(const Point3& other) const
+    {
+        return Vector3(
+            x_ - other.x_,
+            y_ - other.y_,
+            z_ - other.z_);
+    }
+
 private:
     double x_;
     double y_;
