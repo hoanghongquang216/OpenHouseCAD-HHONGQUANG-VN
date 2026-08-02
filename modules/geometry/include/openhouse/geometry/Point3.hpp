b8cdffb401967950e97f8d04ce36fd24c949b0cb
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-08-02 08:32:54 +0700

    Integrate point3 vector operations

--- modules/geometry/include/openhouse/geometry/Point3.hpp
+++ modules/geometry/include/openhouse/geometry/Point3.hpp
@@ -1,16 +1,31 @@
 #pragma once
 
-namespace openhouse::geometry {
+#include <openhouse/geometry/Vector3.hpp>
 
-template<typename T>
-struct Point3 {
-    T x{};
-    T y{};
-    T z{};
+namespace openhouse::geometry
+{
+
+class Point3
+{
+public:
+    constexpr Point3(double x = 0.0, double y = 0.0, double z = 0.0)
+        : x_(x), y_(y), z_(z)
+    {
+    }
+
+    constexpr double X() const { return x_; }
+    constexpr double Y() const { return y_; }
+    constexpr double Z() const { return z_; }
+
+    constexpr Point3 operator+(const Vector3& vector) const
+    {
+        return Point3(x_ + vector.X(), y_ + vector.Y(), z_ + vector.Z());
+    }
+
+private:
+    double x_;
+    double y_;
+    double z_;
 };
-
-using Point3f = Point3<float>;
-using Point3d = Point3<double>;
-using Point3i = Point3<int>;
 
 }
