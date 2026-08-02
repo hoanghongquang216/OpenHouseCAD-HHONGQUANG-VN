3ac5fdcd1d8e28a7086dd7f0874cdcb7502b718a
hoanghongquang216 <hoanghongquang216@gmail.com>
2026-08-02 08:48:09 +0700

    Add property snapshot foundation for transaction history

---
+++ modules/model/include/openhouse/model/PropertySnapshot.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+
+namespace openhouse::model
+{
+
+class PropertySnapshot
+{
+public:
+    PropertySnapshot(std::string name, std::string value)
+        : name_(std::move(name)), value_(std::move(value))
+    {
+    }
+
+    const std::string& Name() const
+    {
+        return name_;
+    }
+
+    const std::string& Value() const
+    {
+        return value_;
+    }
+
+private:
+    std::string name_;
+    std::string value_;
+};
+
+}
